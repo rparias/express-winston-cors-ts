6fe68b92be3c0de5f2a71332f1ae3966d4216373
Ronald Arias <ariasron@hotmail.com>
2022-04-30 20:33:16 -0500

    updates eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   extends: [
-    './node_modules/ts-standard/eslintrc.json',
-  ],
-  rules: {
-    'comma-dangle': [
-      2,
-      'always-multiline',
-    ],
-  },
+    './node_modules/ts-standard/eslintrc.json'
+  ]
 }
